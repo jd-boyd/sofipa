acfa15779b99c4f30761a9d8c17d8c062685a82a
Joshua Boyd <jdboyd@jdboyd.net>
2016-10-02 17:39:31 -0400

    Add jasmine testing to gulp/requirements.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const babel = require('gulp-babel');
-
+const jasmine = require('gulp-jasmin');
 
 gulp.task('css-build', function () {
     gulp.src(['./scss/**/*.css'])
@@ -17,6 +17,18 @@ gulp.task('js-build', function () {
    .pipe(gulp.dest('./dist/'));
 });
 
+gulp.task('test-build', function () {
+   gulp.src(['./tests/**/*.js'])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+   .pipe(gulp.dest('./spec/'));
+});
+
+gulp.task("run-tests", ["js-build", "test-build"], function () {
+    gulp.src('spec/test.js')
+        .pipe(jasmine());
+});
 
 gulp.task('default', ['js-build'],
 	  function() {
